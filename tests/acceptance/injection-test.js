26401aa8df744091c6f0560717a15b1e58a63758
toran billups <toranb@gmail.com>
2015-12-02 11:39:31 -0600

    [DEPENDENCY]: updated ember-cli-auto-register

--- tests/acceptance/injection-test.js
+++ tests/acceptance/injection-test.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
-import lookup from '../helpers/lookup';
 
 var application;
 
@@ -15,22 +14,22 @@ module('Acceptance: Injection', {
 });
 
 test('Objects can be injected with specified name', function(assert) {
-    var controller = lookup('controller:foo');
+    var controller = application.__container__.lookup('controller:foo');
     assert.equal(controller.bar(), 'bar return value');
 });
 
 test('Objects can be injected without name, using name of property', function(assert) {
-    var controller = lookup('controller:wat');
+    var controller = application.__container__.lookup('controller:wat');
     assert.equal(controller.baz(), 'baz return value');
 });
 
 test('injections are found for that specific type', function(assert) {
-    var route = lookup('route:wat');
+    var route = application.__container__.lookup('route:wat');
     assert.equal(route.doTask(), 'task complete');
 });
 
 test('multiple injections of different types can be made into the same object', function(assert) {
-    var controller = lookup('controller:foo');
+    var controller = application.__container__.lookup('controller:foo');
     assert.equal(controller.bar(), 'bar return value');
     assert.equal(controller.startTask(), 'task started');
 });
--- tests/dummy/app/initializers/inject.js
+++ tests/dummy/app/initializers/inject.js
@@ -1,8 +1,9 @@
 import register from 'ember-cli-auto-register/register';
 
-export function initialize(_, application) {
+export function initialize() {
     // this is because of the app inside of the ember-addon
     // this does not need, nor should it be done, in a real app
+    var application = arguments[1] || arguments[0];
     application.name = 'dummy';
 
     register('repos', application);
